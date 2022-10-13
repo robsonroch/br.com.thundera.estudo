53b7e2ca4502cda7202e8a7345d47e532ed2d5ef
Robson <robson.proxycode@gmail.com>
2022-10-13 09:57:36 -0300

    generate project

---
+++ src/main/java/br/com/thunderaestudo/InvesaoInjecaoExemploApplication.java
@@ -0,0 +1,13 @@
+package br.com.thunderaestudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvesaoInjecaoExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvesaoInjecaoExemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thunderaestudo/InvesaoInjecaoExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thunderaestudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvesaoInjecaoExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
